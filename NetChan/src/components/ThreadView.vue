cecaa6a1772c932a3ed2b2f946c77ad6ad38b4fa
Michał Piasecki <michal@elexpress.pl>
2020-09-07 12:32:19 +0200

    Thread view displays correctly (API changed)

--- NetChan/src/components/ThreadView.vue
+++ NetChan/src/components/ThreadView.vue
@@ -1,11 +1,11 @@
 <template>
 	<v-row>
 		<v-col cols="12" class="thread">
-			<div v-for="(post, index) in threadData" :key="post.id">
+			<div v-for="(post, index) in threadData.posts" :key="post.id">
 				<div v-if="index === 0" class="op" :id="'post-' + post.id">
 					<Post :post="post" mode="op" :board="board" />
 				</div>
-				<div v-else class="response" :class="'thread-' + threadData[0].id" :id="'post-' + post.id">
+				<div v-else class="response" :class="'thread-' + threadData.posts[0].id" :id="'post-' + post.id">
 					<Post :post="post" mode="response" :board="board" />
 				</div>
 			</div>
@@ -25,21 +25,21 @@
 			Post
 		},
 		updated: async function () {
-			console.log(this.threadData);
-
 			const replyLinks = document.getElementsByClassName('reply');
 
-			// if reply doesn't exist, remove link
-			for (let x of replyLinks) {
-				const id = x.innerHTML.replace('>>', '').replace('&gt;&gt;', '').replace(' (OP)', '');
-				if (this.isOp(id) === true) {
-					x.innerHTML += ' (OP)';
-				}
-
-				const exists = await this.postExists(id, this.board);
-				if (exists === false) {
-					x.classList.remove('reply');
-					x.classList.add('notfound');
+			// remove links from nonexistent replies
+			if (replyLinks.length > 0) {
+				for (let x of replyLinks) {
+					const id = x.innerHTML.replace('>>', '').replace('&gt;&gt;', '').replace(' (OP)', '');
+					if (this.isOp(id) === true) {
+						x.innerHTML += ' (OP)';
+					}
+	
+					const exists = await this.postExists(id, this.board);
+					if (exists === false) {
+						x.classList.remove('reply');
+						x.classList.add('notfound');
+					}
 				}
 			}
 
